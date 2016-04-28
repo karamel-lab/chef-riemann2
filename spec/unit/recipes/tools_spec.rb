a2cb421fdab3f71a5a8155771531e04f9a824a79
stornqvist <basti94a@gmail.com>
2016-04-28 19:31:05 +0200

    Update tools_spec.rb

--- spec/unit/recipes/tools_spec.rb
+++ spec/unit/recipes/tools_spec.rb
@@ -1,12 +1,12 @@
 #
-# Cookbook Name:: riemann2
+# Cookbook Name:: riemann
 # Spec:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 require 'spec_helper'
 
-describe 'riemann2::tools' do
+describe 'riemann::tools' do
   context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) do
       runner = ChefSpec::ServerRunner.new
